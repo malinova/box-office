a058b27eabf64ff722fa701583f6eaaacf31ef30
Anna Malinova <anna@mlnova.com>
2022-03-09 16:03:17 +0200

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element="test" />
+      <Route path="/starred" element="test2" />
+      <Route path="*" element="This is 404 page" />
+    </Routes>
+  );
 }
 
 export default App;
