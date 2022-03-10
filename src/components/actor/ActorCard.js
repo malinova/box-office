425414082d3f7fc3dbc1b4242eae788974a4a33e
Anna Malinova <anna@mlnova.com>
2022-03-10 19:31:04 +0200

    Installed styled-components, created styles for search card

--- src/components/actor/ActorCard.js
+++ src/components/actor/ActorCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { StyledActorCard } from './ActorCard.styled';
 
 const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
   return (
-    <div>
+    <StyledActorCard>
       <div>
-        <img src={image} alt="actor" />
+        <img src={image} alt="actor" className="img-wrapper" />
       </div>
       <h1>
         {name} {gender ? `(${gender})` : null}
       </h1>
       <p>{country ? `Comes from ${country}` : 'No country known'}</p>
       {birthday ? <p>Born {birthday}</p> : null}
-      <p>{deathday ? `Died ${deathday}` : 'Alive'}</p>
-    </div>
+      <p className="deathday">{deathday ? `Died ${deathday}` : 'Alive'}</p>
+    </StyledActorCard>
   );
 };
 
